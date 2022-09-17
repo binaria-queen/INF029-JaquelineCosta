08b0ff4658bdc2b6d174316327e70121cb394e20
Jaqueline Gabriela Santos Costa <jaqueline.costa@capgemini.com>
2022-09-17 20:32:41 -0300

    Structs e algumas funcoes iniciadas

--- Lista1/RevisitandoLogicaProgramacao/q1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-  //nome, matrícula e o semestre de ingresso no curso, cada um em uma linha
-int main(){
-  printf("Nome: Jaqueline Gabriela\n");
-  printf("Matrícula: 20212160025\n");
-  printf("Semestre: 2022.2\n");
-}
